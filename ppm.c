651b366cb84e2ce870c6564ea44ff8bfd868add3
Tomas Fratrik <tomasfratrik8@gmail.com>
2022-03-11 18:53:17 +0100

    steg-decode.c done | it is working

--- ppm.c
+++ ppm.c
@@ -14,74 +14,11 @@
 
 #define RGB_DEPTH_CONST 255
 #define DATA_LIMIT 8000*8000*3
-// #define IMG_SIZE_LIMIT 8000*8000*3
 
 
 void ppm_free(struct ppm *p){
     free(p);
 }
-
-// struct ppm *ppm_read(const char *filename) {
-
-//     FILE *file = fopen(filename, "r");
-//     if (file == NULL) {
-//         warning_msg("%s : Subor sa nepodarilo otvorit\n", filename);
-//         fclose(file);
-// 		return NULL;
-//     }
-
-//     char imgformat[3];
-//     unsigned int imgxsize,imgysize;
-//     int tmp;
-
-//     if ((fscanf(file, "%s %u %u %d\n", imgformat, &imgxsize, &imgysize, &tmp)) != 4) {
-//         warning_msg("%s : Hlavicka suboru sa nepodarila nacitat\n", filename);
-//         fclose(file);
-// 		return NULL;
-//     }
-
-//     unsigned long imgsize = imgxsize*imgysize*3;
-
-// 	if (imgsize > IMG_SIZE_LIMIT) {
-// 		warning_msg("%s : Velkost obrazku je vacsia ako limit (%d)\n", filename, IMG_SIZE_LIMIT);
-// 		fclose(file);
-// 		return NULL;
-// 	}
-
-//     if (strcmp(imgformat,"P6") != 0) {
-//         warning_msg("%s : Subor ma nespravny format\n", filename);
-//         fclose(file);
-// 		return NULL;
-//     }
-
-//     struct ppm *img = malloc(sizeof(struct ppm)+imgsize);
-
-//     if (img == NULL) {
-//         warning_msg("%s : Alokacia pamate sa nepodarila\n", filename);
-//         fclose(file);
-// 		return NULL;
-//     }
-
-//     img->xsize=imgxsize;
-//     img->ysize=imgysize;
-    
-//     if (fread(img->data, sizeof(char), imgsize, file) != imgsize) {
-//         warning_msg("%s : Velkost obrazku v hlavicke suboru sa nazhoduje s velkostou obrazku v datach\n", filename);
-//         fclose(file);
-//         free(img);
-// 		return NULL;
-//     }
-
-// 	if (fgetc(file) != EOF) {
-// 		warning_msg("%s : Subor nie je ukonceny alebo sa nenacitali vsetky znaky\n", filename);
-// 		fclose(file);
-// 		free(img);
-// 		return NULL;
-// 	}
-
-//     fclose(file);
-//     return img;
-// }
 
 struct ppm * ppm_read(const char * filename){
 
@@ -128,12 +65,6 @@ struct ppm * ppm_read(const char * filename){
 		return NULL;
     }
 
-//     if (fgetc(fp) != EOF) {
-//         warning_msg("%s : Subor nie je ukonceny alebo sa nenacitali vsetky znaky\n", filename);
-//         fclose(fp);
-//         free(p);
-//         return NULL;
-// }
     fclose(fp);
     return p;
 }
