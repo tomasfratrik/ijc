549e6f11a0eedae816495382e2484bb3cd12d0c5
Tomas Fratrik <tomasfratrik8@gmail.com>
2022-03-11 18:52:47 +0100

    steg-decode.c done | it is working

--- ppm.c
+++ ppm.c
@@ -14,17 +14,79 @@
 
 #define RGB_DEPTH_CONST 255
 #define DATA_LIMIT 8000*8000*3
+// #define IMG_SIZE_LIMIT 8000*8000*3
 
 
 void ppm_free(struct ppm *p){
     free(p);
 }
 
+// struct ppm *ppm_read(const char *filename) {
+
+//     FILE *file = fopen(filename, "r");
+//     if (file == NULL) {
+//         warning_msg("%s : Subor sa nepodarilo otvorit\n", filename);
+//         fclose(file);
+// 		return NULL;
+//     }
+
+//     char imgformat[3];
+//     unsigned int imgxsize,imgysize;
+//     int tmp;
+
+//     if ((fscanf(file, "%s %u %u %d\n", imgformat, &imgxsize, &imgysize, &tmp)) != 4) {
+//         warning_msg("%s : Hlavicka suboru sa nepodarila nacitat\n", filename);
+//         fclose(file);
+// 		return NULL;
+//     }
+
+//     unsigned long imgsize = imgxsize*imgysize*3;
+
+// 	if (imgsize > IMG_SIZE_LIMIT) {
+// 		warning_msg("%s : Velkost obrazku je vacsia ako limit (%d)\n", filename, IMG_SIZE_LIMIT);
+// 		fclose(file);
+// 		return NULL;
+// 	}
+
+//     if (strcmp(imgformat,"P6") != 0) {
+//         warning_msg("%s : Subor ma nespravny format\n", filename);
+//         fclose(file);
+// 		return NULL;
+//     }
+
+//     struct ppm *img = malloc(sizeof(struct ppm)+imgsize);
+
+//     if (img == NULL) {
+//         warning_msg("%s : Alokacia pamate sa nepodarila\n", filename);
+//         fclose(file);
+// 		return NULL;
+//     }
+
+//     img->xsize=imgxsize;
+//     img->ysize=imgysize;
+    
+//     if (fread(img->data, sizeof(char), imgsize, file) != imgsize) {
+//         warning_msg("%s : Velkost obrazku v hlavicke suboru sa nazhoduje s velkostou obrazku v datach\n", filename);
+//         fclose(file);
+//         free(img);
+// 		return NULL;
+//     }
+
+// 	if (fgetc(file) != EOF) {
+// 		warning_msg("%s : Subor nie je ukonceny alebo sa nenacitali vsetky znaky\n", filename);
+// 		fclose(file);
+// 		free(img);
+// 		return NULL;
+// 	}
+
+//     fclose(file);
+//     return img;
+// }
+
 struct ppm * ppm_read(const char * filename){
 
 
-    FILE *fp;
-    fp = fopen(filename,"rb");
+    FILE *fp = fopen(filename, "r");
     if(fp == NULL){
         warning_msg("Nepodarilo sa otvorit subor!");
         return NULL;
@@ -33,7 +95,9 @@ struct ppm * ppm_read(const char * filename){
     unsigned long xsize;
     unsigned long ysize;
     char rgb_depth[4];
-    int num_of_params = fscanf(fp,"%s %ld %ld %s",format,&xsize,&ysize,rgb_depth);
+
+    int num_of_params = fscanf(fp,"%s %ld %ld %s\n",format,&xsize,&ysize,rgb_depth);
+    unsigned long size = xsize*ysize*3;
 
 
     if((strcmp(format,"P6") || (RGB_DEPTH_CONST < atoi(rgb_depth))) || num_of_params != 4 ){
@@ -44,15 +108,32 @@ struct ppm * ppm_read(const char * filename){
 
     if((xsize*ysize*3) > DATA_LIMIT){
         warning_msg("Bol prekroceny limit dat!");
+        fclose(fp);
         return NULL;
     }
 
-    struct ppm *p;
-    p = malloc(sizeof(struct ppm)+xsize*ysize*3);
-    if(p = NULL){
+    struct ppm *p = malloc(sizeof(struct ppm)+size);
+    if(p == NULL){
         warning_msg("Nepodarilo sa alokovat pamet!");
         fclose(fp);
         return NULL;
     }
+
+    p->xsize = xsize;
+    p->ysize = ysize;
+    if (fread(p->data, sizeof(char), size, fp) != size) {
+        warning_msg("Nepodarilo sa precitat subor!");
+        fclose(fp);
+        free(p);
+		return NULL;
+    }
+
+//     if (fgetc(fp) != EOF) {
+//         warning_msg("%s : Subor nie je ukonceny alebo sa nenacitali vsetky znaky\n", filename);
+//         fclose(fp);
+//         free(p);
+//         return NULL;
+// }
+    fclose(fp);
     return p;
 }
--- steg-decode.c
+++ steg-decode.c
@@ -9,9 +9,43 @@
 #include <stdlib.h>
 #include "ppm.h"
 #include "bitset.h"
+#include"eratosthenes.h"
 
-int main(){
- ppm_read("du1-obrazek.ppm");
+int main() {
+
+    struct ppm * ppm_p = ppm_read("du1-obrazek.ppm");
+    if(ppm_p == NULL){
+        error_exit("Nepodarilo sa precitat ppm format!");
+    }
+
+    unsigned long size = ppm_p->xsize*ppm_p->ysize*3;
+    bitset_alloc(msgArray, size);
+	Eratosthenes(msgArray);
+
+	bitset_create(single_char, 8); 
+    int counter = 0;
+
+    for (unsigned long i = 29;i < size ;i++) {
+        if (bitset_getbit(msgArray,i) == 0) {
+            int bit = ppm_p->data[i] & 1;
+            bitset_setbit(single_char, counter, bit);
+            counter++;
+            if(counter == 8) {
+				if(single_char[1] == '\0') {
+					printf("\n");
+					ppm_free(ppm_p);
+					bitset_free(msgArray);
+					return 0;
+				}
+                putchar((char) single_char[1]);
+				counter = 0;
+            }
+        }
+    }
+
+
+    bitset_free(msgArray);
+    ppm_free(ppm_p);
+    
+    return 0;
 }
-
-
